01584238db5b654c527d7a231393ca9c76ee4517
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-04-10 15:23:44 +0500

    User Authentication using JWT and Store Token in MongoDB

--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 require("../db/conn");
 const User = require("../model/userSchema");
@@ -50,6 +51,7 @@ router.post("/register", async (req, res) => {
 
 router.post("/signin", async (req, res) => {
   try {
+    let token;
     const { email, password } = req.body;
 
     if (!email || !password) {
@@ -62,6 +64,9 @@ router.post("/signin", async (req, res) => {
 
     if (userLogin) {
       const isMatch = await bcrypt.compare(password, userLogin.password);
+
+      token = await userLogin.generateAuthToken();
+      console.log(token);
 
       if (!isMatch) {
         res.status(400).json({ error: "Invalid Credentials" });
